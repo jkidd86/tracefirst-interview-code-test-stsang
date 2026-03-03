e66f93fef75394a2a2f1ac12b7e5cb428c870f06
abdullah janjua <ajanjua@tracefirst.com>
2026-03-03 15:19:03 +0000

    [#2] feat: adding the locales and modal validations

--- app/models/veterinarian.rb
+++ app/models/veterinarian.rb
@@ -1,3 +1,7 @@
 class Veterinarian < ApplicationRecord
   has_many :tests, dependent: :restrict_with_exception
+
+  PHONE_REGEXP = /\A\+[1-9]\d{6,14}\z/
+
+  validates :number, format: { with: PHONE_REGEXP }, on: :create
 end
