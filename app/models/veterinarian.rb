008ca8fe4ab7dc1602fc8fa3ab427639f9e39f9a
stsang <123456+username@users.noreply.github.com>
2026-03-07 13:30:02 +0000

    Validate new Veterinarian number and make sure its international

--- app/models/veterinarian.rb
+++ app/models/veterinarian.rb
@@ -3,4 +3,7 @@ class Veterinarian < ApplicationRecord
 
 #   ADD IN NEW SCOP WHERE ACTIVE ONLY SHOWS WHEN is_deleted IS FLASE OR EMPTY
   scope :active, -> { where(is_deleted: [false, nil] )}
+
+#   ADD VALID INTERNATIONAL NUMBER CHECK
+  validates :number, format: { with:  /\A\+[1-9]\d{1,14}\z/, message: " must be valid international phone number format"}
 end
